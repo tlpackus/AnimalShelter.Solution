0bc489e3223e909731fa0c906a973877acfe62e6
tlpackus <tlpackus@gmail.com>
2021-07-10 19:50:26 -0700

    add put and delete routes to controller

--- AnimalShelter/Controllers/PetsController.cs
+++ AnimalShelter/Controllers/PetsController.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -43,5 +44,54 @@ namespace AnimalShelter.Controllers
 
       return CreatedAtAction(nameof(GetPet), new { id = pet.PetId }, pet);
     }
+    [HttpPut("{id}")]
+    public async Task<IActionResult> Put(int id, Pet pet)
+    {
+      if (id != pet.PetId)
+      {
+        return BadRequest();
+      }
+
+      _db.Entry(pet).State = EntityState.Modified;
+
+      try
+      {
+        await _db.SaveChangesAsync();
+      }
+      catch (DbUpdateConcurrencyException)
+      {
+        if (!PetExists(id))
+        {
+          return NotFound();
+        }
+        else
+        {
+          throw;
+        }
+      }
+
+      return NoContent();
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeletePet(int id)
+    {
+      var pet = await _db.Pets.FindAsync(id);
+      if (pet == null)
+      {
+        return NotFound();
+      }
+
+      _db.Pets.Remove(pet);
+      await _db.SaveChangesAsync();
+
+      return NoContent();
+    }
+
+    private bool PetExists(int id)
+    {
+      return _db.Pets.Any(e => e.PetId == id);
+    }
+
   }
 }
