23c7495664b1a26e8ae982bd649b1f972ae15d64
tlpackus <tlpackus@gmail.com>
2021-07-10 20:07:47 -0700

    add validations to require a species entry and a range to age entrys

---
+++ AnimalShelter/Migrations/20210711030558_Validations.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AnimalShelter.Migrations
+{
+    public partial class Validations : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Species",
+                table: "Pets",
+                type: "longtext CHARACTER SET utf8mb4",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "longtext CHARACTER SET utf8mb4",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Species",
+                table: "Pets",
+                type: "longtext CHARACTER SET utf8mb4",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "longtext CHARACTER SET utf8mb4");
+        }
+    }
+}
--- AnimalShelter/Models/Pet.cs
+++ AnimalShelter/Models/Pet.cs
@@ -1,10 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace AnimalShelter.Models
 {
   public class Pet
   {
     public int PetId { get; set; }
     public string Name { get; set; }
+    [Required]
     public string Species { get; set; }
+    [Range(0, 300, ErrorMessage = "Age must be between 0 and 300.")]
     public int Age { get; set; }
     public string Gender { get; set; }
   }
