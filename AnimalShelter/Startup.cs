e30db917aed025ffd6be30a3c7e2f7919a9ea0fb
tlpackus <tlpackus@gmail.com>
2021-07-10 18:25:22 -0700

    correct faults in the template and  update the projects launch url

--- AnimalShelter/Startup.cs
+++ AnimalShelter/Startup.cs
@@ -44,7 +44,7 @@ namespace AnimalShelter
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "AnimalShelter v1"));
             }
 
-            app.UseHttpsRedirection();
+            // app.UseHttpsRedirection();
 
             app.UseRouting();
 
