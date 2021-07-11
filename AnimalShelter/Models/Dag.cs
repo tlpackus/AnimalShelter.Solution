bdfbff7c256735e412abcc31b3a1a494b14f8907
tlpackus <tlpackus@gmail.com>
2021-07-10 18:34:36 -0700

    add pet class with getters and setters for the for potential user inputs

---
+++ AnimalShelter/Models/Dag.cs
@@ -0,0 +1,11 @@
+namespace AnimalShelter.Models
+{
+  public class Pet
+  {
+    public int PetId { get; set; }
+    public string Name { get; set; }
+    public string Species { get; set; }
+    public string Age { get; set; }
+    public string Gender { get; set; }
+  }
+}
